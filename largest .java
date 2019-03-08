fd4dd9a5f4fdc7ac6ab22ebe9f703a5665ca4242
gowsicse <47376791+gowsicse@users.noreply.github.com>
2019-03-08 02:15:20 -0800

    Create largest .java

---
+++ largest .java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+ class ideone
+{
+      public static void main(String aa[]){
+            int a,b,c;
+            Scanner s=new Scanner(System.in);
+             a=s.nextInt();
+             b=s.nextInt();
+             c=s.nextInt();
+             if(a>b&&a>c)
+{
+                 System.out.printf("%d",a);
+          }
+           else if(b>c)
+{
+                System.out.printf("%d",b);
+           }
+            else
+{
+                  System.out.printf("%d",c);
+            }
+           }  
+      }
